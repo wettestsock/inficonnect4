265553c3dfad5657f10051addc4d92a45c103f51
wettestsock <evilsharkeater@gmail.com>
2023-12-18 21:05:33 -0500

    replace boxes with normal charatcers

--- main.cpp
+++ main.cpp
@@ -7,16 +7,16 @@
 
 
 int main(){
-//std::cout << "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"<<N;
-//std::cout << "  █ █▄ █ █▀▀ █ █▄ █ █ ▀█▀ █▀▀   █▀▀ █▀█ █▄ █ █▄ █ █▀▀ █▀▀ ▀█▀   █ █"<<N;
-//std::cout << "  █ █ ▀█ █▀  █ █ ▀█ █  █  ██▄   █▄▄ █▄█ █ ▀█ █ ▀█ ██▄ █▄▄  █    ▀▀█"<<N;
+//std::cout << "-------------------------------------------------------------------"<<N;
+//std::cout << "  | |▄ | |-- | |▄ | | -|- |--   |-- |-| |▄ | |▄ | |-- |-- -|-   | |"<<N;
+//std::cout << "  | | -| |-  | | -| |  |  ||▄   |▄▄ |▄| | -| | -| ||▄ |▄▄  |    --|"<<N;
 //std::cout << "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄"<<N;
 
 std::cout << N << N << big_text(" infinite connect 4 ") ;
 
-std::cout << "█\n█ Connect 4 but you decide how to play it." << N << "█ I decided to make a more advanced, OOP, Connect 4." << N;
-std::cout << "█ Planning on training a neural network that"<< N << "█ can fight itself in 100+ columns/rows." << N << "█ "<< N;
-std::cout << "█ Highly scalable and foolproof."<< N << "█ You won't crash this thing." << N << "█" << N;
+std::cout << "|\n| Connect 4 but you decide how to play it." << N << "| I decided to make a more advanced, OOP, Connect 4." << N;
+std::cout << "| Planning on training a neural network that"<< N << "| can fight itself in 100+ columns/rows." << N << "| "<< N;
+std::cout << "| Highly scalable and foolproof."<< N << "| You won't crash this thing." << N << '|' << N;
     int player_num; //amount of players
     int columns, rows;  //rows and columns input
     std::vector<char> char_id;   //all player char ids
@@ -24,16 +24,16 @@ std::cout << "█ Highly scalable and foolproof."<< N << "█ You won't crash th
 
 
 
-    std::cout << "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << N;
+    std::cout << "|----------------------" << N;
     while (true)
     {
-        std::cout << "█ Number of players: ";
+        std::cout << "| Number of players: ";
         std::cin>> player_num;
 
 
         //conditions, fail safe
         if(!std::cin){
-            std::cout<< "█ Invalid number of players." << N << "█" << N;
+            std::cout<< "| Invalid number of players." << N << '|' << N;
             cin_clear(std::cin);
             continue;
         } else if(player_num==0){
@@ -41,7 +41,7 @@ std::cout << "█ Highly scalable and foolproof."<< N << "█ You won't crash th
             return 0;
         
         } else if (player_num <2){
-            std::cout<< "█ At least 2 players are required." << N << "█" << N;
+            std::cout<< "| At least 2 players are required." << N << '|' << N;
             continue;
         }
 
@@ -52,18 +52,18 @@ std::cout << "█ Highly scalable and foolproof."<< N << "█ You won't crash th
     }
 
 
-    std::cout << "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << N;
+    std::cout << "|----------------------" << N;
     while (true)
     {
-        std::cout << "█ Row size: ";
+        std::cout << "| Row size: ";
         std::cin>> rows;
 
         if(!std::cin){
-            std::cout<< "█ Invalid row size." << N << "█" << N;
+            std::cout<< "| Invalid row size." << N << '|' << N;
             cin_clear(std::cin);
             continue;
         } else if (rows<4){
-            std::cout<< "█ At least 4 rows are required." << N << "█" << N;
+            std::cout<< "| At least 4 rows are required." << N << '|' << N;
             continue;
         }
 
@@ -72,18 +72,18 @@ std::cout << "█ Highly scalable and foolproof."<< N << "█ You won't crash th
     }
 
 
-    std::cout << "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << N;
+    std::cout << "|----------------------" << N;
     while (true)
     {
-        std::cout << "█ Column size: ";
+        std::cout << "| Column size: ";
         std::cin>> columns;
 
         if(!std::cin){
-            std::cout<< "█ Invalid column size." << N << "█" << N;
+            std::cout<< "| Invalid column size." << N << '|' << N;
             cin_clear(std::cin);
             continue;
         } else if (columns<4){
-            std::cout<< "█ At least 4 columns are required." << N << "█" << N;
+            std::cout<< "| At least 4 columns are required." << N << '|' << N;
             continue;
         }
 
@@ -100,17 +100,17 @@ std::cout << "█ Highly scalable and foolproof."<< N << "█ You won't crash th
         std::string temp;
 
         //prints the player
-        std::cout << "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << N;
-        std::cout<< "█ PLAYER " << i+1 << N;
+        std::cout << "|----------------------" << N;
+        std::cout<< "| PLAYER " << i+1 << N;
 
         while (true)
         {
-            std::cout<< "█ Name: ";
+            std::cout<< "| Name: ";
             std::cin>> temp;
 
             //if name's already taken
             if(in_list(temp, name_id)){
-                std::cout<<"█ Duplicate name. Try again." << N;
+                std::cout<<"| Duplicate name. Try again." << N;
                 continue;
             }
             //pushes the name and its associated character
@@ -125,25 +125,25 @@ std::cout << "█ Highly scalable and foolproof."<< N << "█ You won't crash th
         while (true)
         {
             
-            std::cout<< "█\t" << "(ex: "<< (char)toupper(temp[0]) << ", H, J, etc)"<< N << "█ Character identifier: ";
+            std::cout<< "|\t" << "(ex: "<< (char)toupper(temp[0]) << ", H, J, etc)"<< N << "| Character identifier: ";
             std::cin>> temp;
 
             if (in_list((char)toupper(temp[0]), char_id))
             {
-                std::cout<<"█ Duplicate character. Try again." << N;
+                std::cout<<"| Duplicate character. Try again." << N;
                 continue;
             }
             //default
             char_id.push_back(toupper(temp[0]));
             break;
         }
-        std::cout << "█" << N << "█ NAME: " << name_id[i] << "  ID: " << char_id[i] << N;
+        std::cout << '|' << N << "| NAME: " << name_id[i] << "  ID: " << char_id[i] << N;
 
         //if(z_check(temp)) return 0; // DEBUG DELETE LATER
 
     }
 
-    std::cout << "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << N;
+    std::cout << "-----------------------" << N;
 
     //INITS THE BOARD
     board a(rows, columns, char_id);
@@ -155,16 +155,16 @@ std::cout << "█ Highly scalable and foolproof."<< N << "█ You won't crash th
         for (int i = 0; i < char_id.size(); i++)
         {   
             int move;
-            std::cout << "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << N;
+            std::cout << "|----------------------" << N;
             while (true)
             {
-                std::cout<<"█ " << name_id[i] << " (identifier " << char_id[i] << "), move: ";
+                std::cout<<"| " << name_id[i] << " (identifier " << char_id[i] << "), move: ";
                 std::cin>> move;
 
                 //if invalid
                 if(!std::cin)  
                 {
-                    std::cout<< "█ Invalid move. Try again." << N << "█" << N;
+                    std::cout<< "| Invalid move. Try again." << N << '|' << N;
                     cin_clear(std::cin);
                     continue;
                 
@@ -174,16 +174,16 @@ std::cout << "█ Highly scalable and foolproof."<< N << "█ You won't crash th
 
                 //if input out of range
                 } else if (move < 1 || move > columns) {  
-                    std::cout << "█ Out of range. Try again." << N << "█" << N;
+                    std::cout << "| Out of range. Try again." << N << '|' << N;
                     continue;
                     
                 // if a column is full (top one is taken)
                 } else if (a.the_board[0][move-1] != ' '){  
-                    std::cout << "█ Column is full. Try another one." << N << "█" << N;
+                    std::cout << "| Column is full. Try another one." << N << '|' << N;
                     continue;
                 }
-                std::cout << "█" << N;
-                std::cout << "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << N;
+                std::cout << '|' << N;
+                std::cout << "-----------------------" << N;
                 break;
             }
 
